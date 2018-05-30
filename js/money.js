86011bc5d39d71abe8ec423139a55cb12f8f2b1e
Ray Villaraza <rvillaraza9362@fs1477mbpro.avaflipswitch.com>
2018-05-30 15:51:58 -0700

    save

---
+++ js/money.js
@@ -0,0 +1,36 @@
+debugger
+
+var isMomHappy = true;
+
+// Promise
+var willIGetNewPhone = new Promise(
+    (resolve, reject) => { // fat arrow
+        if (isMomHappy) {
+            const phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone);
+        } else {
+            const reason = new Error('mom is not happy');
+            reject(reason);
+        }
+
+    }
+);
+
+var showOff = function (phone) {
+    const message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+    return Promise.resolve(message);
+};
+
+// call our promise
+var askMom = function () {
+    willIGetNewPhone
+        .then(showOff)
+        .then(fulfilled => console.log(fulfilled)) // fat arrow
+        .catch(error => console.log(error.message)); // fat arrow
+};
+
+askMom();
