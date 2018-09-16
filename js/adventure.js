75466f6b0c29252b8a95d5c32fc0fe3d3d83fea5
Ray Villaraza <ray.villaraza@gmail.com>
2018-09-15 22:49:36 -0700

    navigation

--- js/adventure.js
+++ js/adventure.js
@@ -1,1 +1,36 @@
-console.log("Hello!");
+window.addEventListener('click', (e) => {
+
+    let dataAttributeName = e.target.dataset.attributeName;
+
+    if (dataAttributeName === 'home') {
+        console.log("Load Home!");
+    }
+
+    if (dataAttributeName === 'lodging') {
+        console.log("Load Lodging!");
+    }
+
+    if (dataAttributeName === 'getting-there') {
+        console.log("Load Getting There!");
+    }
+
+    if (dataAttributeName === 'book-your-adventure') {
+        console.log("Load Book!");
+    }
+
+    if (dataAttributeName === 'schedule') {
+        console.log("Load Schedule!");
+    }
+
+    if (dataAttributeName === 'gallery') {
+        console.log("Load Gallery!");
+    }
+
+    if (dataAttributeName === 'contact-us') {
+        console.log("Load Contact Us!");
+    }
+
+    if (dataAttributeName === 'faqs') {
+        console.log("Load FAQs!");
+    }
+})
