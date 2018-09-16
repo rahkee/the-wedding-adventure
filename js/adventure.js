5166d779a3fd10809b3b394b1c26d995b50a0e93
Ray Villaraza <ray.villaraza@gmail.com>
2018-09-16 15:33:50 -0700

    navigation is working smoothly

--- js/adventure.js
+++ js/adventure.js
@@ -1,56 +1,42 @@
 document.onreadystatechange = () => {
 
     if (document.readyState === 'complete') {
-
-        // Get all buttons
         const navButtons = document.querySelectorAll('button');
 
-        // Add a listener to all buttons
         navButtons.forEach(button => {
             button.addEventListener('click', showPage);
         });
 
-        // Get all main elements
-        const mainElements = document.querySelectorAll('main');
+        hideAllMainElements();
 
-        // Hide all main elements
-        mainElements.forEach(mainElement => {
-            mainElement.classList.remove('show');
-            mainElement.classList.add('delete');
-        })
-
-        // Show the home page
         const homePage = document.querySelector('.home');
         homePage.classList.remove('delete');
         homePage.classList.add('show');
     }
-
 };
 
-function showPage (e) {
-    // Get the button attribute name and convert it to string (".class")
-    const elementClass = '.' + e.target.dataset.attributeName;
-
-    // Find the main element to show using the (".class")
+function showPage (evt) {
+    const elementClass = '.' + evt.target.dataset.attributeName;
     const visibleMainElement = document.querySelector(elementClass);
 
-    // Get all main elements
+    hideAllMainElements();
+      
+    visibleMainElement.classList.remove('delete');
+    visibleMainElement.classList.add('show');
+}
+
+function hideAllMainElements() {
     const mainElements = document.querySelectorAll('main');
 
-    TODO
-
-    // Hide all main elements
     mainElements.forEach(mainElement => {
-        setTimeout(() => {
-            mainElement.classList.add('delete');
-        }, 100);
 
-        mainElement.classList.remove('show');
+        if (mainElement.classList.contains('show')) {
+            mainElement.classList.remove('show');
+
+            setTimeout(() => {
+                mainElement.classList.add('delete');
+            }, 400);
+            // TODO: Convert milliseconds to stylesheet var
+        }
     })
-
-    setTimeout(() => {
-        visibleMainElement.classList.add('show');
-    }, 100);
-
-    visibleMainElement.classList.remove('delete');
 }
